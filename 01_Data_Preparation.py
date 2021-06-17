f8268ad998ec7cddbbd9637052fbeddf745bcb0e
Manuel Popp <34488159+ManuelPopp@users.noreply.github.com>
2021-06-18 00:39:15 +0200

    Update 01_Data_Preparation.py

--- 01_Data_Preparation.py
+++ 01_Data_Preparation.py
@@ -8,12 +8,12 @@ Loading data and preparing the training tiles.
 """
 __author__ = "Manuel R. Popp"
 
-### select plots to use
+## select plots to use
 use_plots = ["B3_4"]
 year = "03_2021"
 no_data_class = True
 
-#### AcrGIS Online log-in
+## AcrGIS Online log-in
 if not os.path.exists(os.path.join(wd, "gis", "pw.txt")):
     from cryptography.fernet import Fernet
     import urllib
@@ -22,7 +22,7 @@ if not os.path.exists(os.path.join(wd, "gis", "pw.txt")):
     for line in file:
         l = line.decode("utf-8")
         b = bytes(l, "utf-8")
-        key = b"If authorized: INSERT KEY"
+        key = b"5v-V22Z8KfJjs6XlcMaO1OxDhx5mXxC0YMuKTI67pSs="
         fernet = Fernet(key)
     login = eval(fernet.decrypt(b).decode())
     key = Fernet.generate_key()
@@ -30,25 +30,7 @@ if not os.path.exists(os.path.join(wd, "gis", "pw.txt")):
 else:
     login = open(os.path.join(wd, "gis", "pw.txt"), "r").readlines()
 
-import arcgis
-from arcgis.gis import GIS
-gis = GIS(None, login[0].rstrip(), login[1], verify_cert = False)
-def dir_shp(plot_id = None, myear = None):
-    if myear == None:
-        if plot_id == None:
-            return(dir_dat("shp"))
-        else:
-            print("Option myear missing but required.")
-    else:
-        if plot_id == None:
-            return(os.path.join(dir_dat("shp"), myear))
-        else:
-            os.makedirs(os.path.join(dir_dat("shp"), myear, plot_id),
-                        exist_ok = True)
-            return(os.path.join(dir_dat("shp"), myear, plot_id))
-del login
-
-import arcgis
+import arcgis, math
 from arcgis.gis import GIS
 gis = GIS(None, login[0].rstrip(), login[1], verify_cert = False)
 def dir_shp(plot_id = None, myear = None):
@@ -68,7 +50,7 @@ del login
 
 def downloadShapefiles(plot_id, path, dateTime = None):
     try:
-        # Search items by username
+        ## Search items by username
         cont = gis.content.search("owner:{0}".format("manuel.popp_KIT"))
         for i in range(len(cont)):
             shp = cont[i]
@@ -111,7 +93,7 @@ def downloadShapefiles(plot_id, path, dateTime = None):
 def get_classes(path):
     from osgeo import ogr
     drv = ogr.GetDriverByName("ESRI Shapefile")
-    # read existing classes
+    ## read existing classes
     files = list(pathlib.Path(path).glob("**/*.shp"))
     shapefiles = []
     for file in files:
@@ -137,7 +119,7 @@ def get_classes(path):
 def encode_classes(path, classes):
     from osgeo import ogr
     drv = ogr.GetDriverByName("ESRI Shapefile")
-    # create new field
+    ## create new field
     dataSource = drv.Open(path, 1)
     FDef = ogr.FieldDefn("Class", ogr.OFTInteger)
     layer = dataSource.GetLayer()
@@ -163,7 +145,7 @@ def encode_classes(path, classes):
         wr.writerows(zip(["ClassID"], ["Encoded"]))
         wr.writerows(zip(classes, range(len(classes))))
 
-### get data and create masks and tiles
+## get data and create masks and tiles
 import numpy as np
 from osgeo import gdal, ogr, osr
 from osgeo.gdal_array import CopyDatasetInfo, BandReadAsArray
@@ -192,7 +174,7 @@ def blacken(data, mask, NoDataVal, outFile = "/vsimem/Blackened.tif"):
     return new
 
 def check_version(file, derived_from):
-    # expects pathlib path list
+    ### expects pathlib path list
     if len(file) < 1:
         return None
     else:
@@ -203,8 +185,36 @@ def check_version(file, derived_from):
             from_mod = derived_from[0].stat().st_mtime
             return file_mod > from_mod
 
+## download image extents shapefile
+if os.path.isfile(os.path.join(dir_shp("Extents", myear = year),
+                               "Extents.shp")):
+    last_modified = None
+else:
+    last_modified = pathlib.Path(dir_shp("Extents", myear = year),
+                               "Extents.shp").stat().st_mtime
+downloadShapefiles(plot_id = "Extents",
+                           path = dir_shp("Extents", myear = year),
+                           dateTime = last_modified)
+Ext = os.path.join(dir_shp("Extents", myear = year), "Extents.shp")
+ExtUTM = s[:len(s)-4] + "_UTM.shp"
+## transform extents shapefile to UTM Zone 35S
+drv = ogr.GetDriverByName("ESRI Shapefile")
+if os.path.exists(ExtUTM):
+    drv.DeleteDataSource(ExtUTM)
+srcDS = gdal.OpenEx(Ext)
+crs = osr.SpatialReference()
+crs.ImportFromEPSG(32735)
+ds = gdal.VectorTranslate(ExtUTM, srcDS, format = "ESRI Shapefile",
+                          dstSRS = crs, reproject = True)
+ds = None
+srcDS = None
+### write .prj file
+with open(f"{os.path.splitext(sUTM)[0]}.prj", "w") as f:
+    f.write(crs1)
+
+# run for each image or plot
 for plot in use_plots:
-    # update shapefile if required
+    ### update shapefile if required
     if len(list(pathlib.Path(dir_shp(plot, myear = year)) \
                 .glob("**/*" + plot + ".shp"))) < 1:
         last_modified = None
@@ -216,30 +226,30 @@ for plot in use_plots:
                            dateTime = last_modified)
     shp_path = list(pathlib.Path(dir_shp(plot, myear = year)) \
                     .glob("**/*" + plot + ".shp"))[0]
-# get class IDs
+## get class IDs
 classes_decoded = get_classes(dir_shp(myear = year))
 classes = range(len(classes_decoded))
 NoDataValue = max(classes) + 1
-save_var(variables = [classes, classes_decoded, NoDataValue],
+save_var(variables = [classes, classes_decoded, NoDataValue, no_data_class],
          name = "ClassIDs")
 
 for plot in use_plots:
     encode_classes(path = os.path.join(shp_path), classes = classes_decoded)
-    # tif and shp path
+    ### tif and shp path
     t = os.path.join(dir_omk(plot)[0])
     plib = pathlib.Path(dir_shp(plot, myear = year))
-    # check if shapefile was transformed to UTM
+    ### check if shapefile was transformed to UTM
     if check_version(file = list(pathlib.Path(dir_shp(plot, myear = year)). \
                               glob("**/*_UTM.shp")),
                   derived_from = list(
                       pathlib.Path(dir_shp(plot, myear = year)). \
                               glob("**/*" + plot + ".shp"))) in (None, False):
         if os.path.exists(t):
-            # open orthomosaic
+            ### open orthomosaic
             tif = gdal.Open(t)
             crs1 = tif.GetProjectionRef()#GetSpatialRef()#.GetProjection()
             tif = None
-            # reproject shapefile
+            ### reproject shapefile
             s = os.path.join(list(plib.glob("**/*.shp"))[0])
             sUTM = s[:len(s)-4] + "_UTM.shp"
             drv = ogr.GetDriverByName("ESRI Shapefile")
@@ -250,7 +260,7 @@ for plot in use_plots:
                                       dstSRS = crs1, reproject = True)
             ds = None
             srcDS = None
-            # write .prj file
+            ### write .prj file
             with open(f"{os.path.splitext(sUTM)[0]}.prj", "w") as f:
                 f.write(crs1)
         else:
@@ -259,7 +269,7 @@ for plot in use_plots:
                      derived_from = list(pathlib.Path(dir_shp(plot,
                                                               myear = year)). \
                               glob("**/*_UTM.shp"))) in (None, False):
-        # create mask
+        ## create mask
         sUTM = os.path.join(list(plib.glob("**/*_UTM.shp"))[0])
         drv = ogr.GetDriverByName("ESRI Shapefile")
         shp = drv.Open(sUTM)
@@ -307,15 +317,15 @@ for plot in use_plots:
     else:
         t_crop = dir_omk(plot, type_ext = "_CROP")
         m = dir_omk(plot, type_ext = "_MASK")
-    # check if training tiles were generated
+    ### check if training tiles were generated previously and are up-to-date
     if check_version(file = list(pathlib.Path(dir_tls(plot_id = plot,
                                                    myear = year)) \
                               .glob("**/*_y.tif")),
                   derived_from = list(pathlib.Path(dir_omk(myear = year)). \
                                       glob("**/*MASK.tif"))
                   ) in (None, False):
-        ### create tiles
-        # blacken pixels not assigned to any class
+        ## create tiles
+        ### blacken pixels not assigned to any class
         if no_data_class:
             tif = blacken(data = t_crop, mask = m, NoDataVal = NoDataValue)
         else:
@@ -324,19 +334,25 @@ for plot in use_plots:
         r = mask.RasterYSize
         c = mask.RasterXSize
         geo_tf = mask.GetGeoTransform()
-        xmin = geo_tf[0]
-        ymax = geo_tf[3]
+        Xmin = geo_tf[0]
+        Ymax = geo_tf[3]
         res = geo_tf[1]
-        ytiles = int(r/imgr)
-        xtiles = int(c/imgc)
+        ytiles = math.floor(r/imgr)
+        xtiles = math.floor(c/imgc)
         ypx = ytiles*int(imgr)
         xpx = xtiles*int(imgc)
-        # walk North, since we're in the southern hemisphere
+        ### offset the raster so the unused area is equally distributed at all
+        ### sides of the rectangle
+        yoffset = 0.5*(r - ypx)
+        xoffset = 0.5*(r - xpx)
+        ymax = Ymax - yoffset
+        xmax = Xmax - xffset
+        ### walk North, since we're in the southern hemisphere
         ycrds = [ymax - res*imgr*m for m in range(ytiles+1)]
         xcrds = [xmin + res*imgc*n for n in range(xtiles+1)]
         os.makedirs(dir_tls(myear = year, dset = "X"), exist_ok = True)
         os.makedirs(dir_tls(myear = year, dset = "y"), exist_ok = True)
-        # delete old files
+        ### delete old files
         old_X_tiles = list(pathlib.Path(dir_tls(myear = year, dset = "X")) \
                            .glob("**/" + plot + "*"))
         old_y_tiles = list(pathlib.Path(dir_tls(myear = year, dset = "y")) \
@@ -362,7 +378,7 @@ for plot in use_plots:
                 counter = counter + 1
         tif = None
         mask = None
-# delete all-black tiles
+### delete all-black tiles
 y_tiles = list(pathlib.Path(dir_tls(myear = year, dset = "y")) \
                .glob("**/*.tif"))
 all_black = []
@@ -380,22 +396,22 @@ for i in all_black:
     os.remove(corr_X)
     os.remove(os.path.join(i))
 
-#### split in training and validation set
-# set share of validation tiles
+## split in training and validation set
+### set share of validation tiles
 tst_share = 0.1
 val_share = 0.25
 N_tot = len(list(pathlib.Path(dir_tls(myear = year, dset = "X")) \
                          .glob("**/*.tif")))
-# check if tiles are up-to-date
+### check if tiles are up-to-date
 tiles_Xmfd = pathlib.Path(dir_tls(myear = year, dset = "X")).stat() \
     .st_mtime
-# initialize, if files are missing
+### initialize, if files are missing
 import shutil
 for SET in ["tst", "val"]:
     if not os.path.exists(dir_tls(myear = year, dset = "X_" + SET)):
         copy_new = True
     else:
-        # delete old, if train set is newer than tst/val set
+        ##### delete old, if train set is newer than tst/val set
         if tiles_Xmfd > pathlib.Path(dir_tls(myear = year,
                                         dset = "X_" + SET)).stat().st_mtime:
             copy_new = True
