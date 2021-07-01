c4c08d4ffdc4112f45e540b1bacc7be1a0226286
Manuel Popp <34488159+ManuelPopp@users.noreply.github.com>
2021-07-01 23:56:10 +0200

    Update and rename 04_Models_FCDenseNet.py to A4_Models_FCDenseNet.py

--- A4_Models_FCDenseNet.py
+++ A4_Models_FCDenseNet.py
@@ -3,7 +3,8 @@
 Created on Sat May 29 18:48:59 2021
 
 @author: Manuel
-This code implements the Fully Convolutional DenseNet described in
+This code implements the Fully Convolutional DenseNetproposed by
+Jegou et al. (2017) in the following publication
 https://arxiv.org/abs/1611.09326 using tf.Keras.
 The original implementation using Theano and Lasagne was made public on
 https://github.com/SimJeg/FC-DenseNet/blob/master/FC-DenseNet.py
@@ -12,11 +13,12 @@ When using this as standalone script, import the following hashed-out packages
 # import tensorflow as tf
 # from tensorflow import keras as ks
 
-# Define blocks
+# define blocks
 def BN_ReLU_Conv(inputs, n_filters, filter_size = 3, dropout_p = 0.2):
     l = ks.layers.BatchNormalization()(inputs)
     l = ks.layers.Activation("relu")(l)
-    l = ks.layers.Conv2D(n_filters, filter_size, activation = None, padding = "same", 
+    l = ks.layers.Conv2D(n_filters, filter_size, activation = None,
+                         padding = "same", 
                          kernel_initializer = 'he_uniform') (l)
     if dropout_p != 0.0:
         l = ks.layers.Dropout(dropout_p)(l)
@@ -61,7 +63,7 @@ def FCDense(n_classes, input_shape = (imgr, imgc, imgdim),
     :param dropout_p: dropout rate applied after each convolution
         (0. for not using)
     """
-    # Check n_layers_per_block setting
+    # check n_layers_per_block setting
     if type(n_layers_per_block) == list:
         assert(len(n_layers_per_block) == 2*n_pool + 1)
     elif type(n_layers_per_block) == int:
@@ -71,7 +73,7 @@ def FCDense(n_classes, input_shape = (imgr, imgc, imgdim),
     # Input layer, m = 3
     inputz = tf.keras.layers.Input(shape = input_shape)
     
-    # First convolution; store feature maps in the Tiramisu
+    # first convolution; store feature maps in the Tiramisu
     # 3 x 3 convolution, m = 48
     Tiramisu = ks.layers.Conv2D(filters = n_filters_first_conv,
                                 kernel_size = (3, 3), strides = (1, 1),
@@ -81,46 +83,46 @@ def FCDense(n_classes, input_shape = (imgr, imgc, imgdim),
                                 )(inputz)
     n_filters = n_filters_first_conv
     
-    # Downsampling path, n*(dense block + transition down)
+    # downsampling path, n*(dense block + transition down)
     skip_connection_list = []
     
     for i in range(n_pool):
-        ## Dense block
+        ## dense block
         for j in range(n_layers_per_block[i]):
             ### Compute new feature maps
             l = BN_ReLU_Conv(Tiramisu, growth_rate, dropout_p=dropout_p)
-            ### And stack it---the Tiramisu is growing
+            ### and stack it---the Tiramisu is growing
             Tiramisu = ks.layers.concatenate([Tiramisu, l])
             n_filters += growth_rate
-        ## Store Tiramisu in skip_connections list
+        ## store Tiramisu in skip_connections list
         skip_connection_list.append(Tiramisu)
-        ## Transition Down
+        ## transition Down
         Tiramisu = TransitionDown(Tiramisu, n_filters, dropout_p)
     skip_connection_list = skip_connection_list[::-1]
     
-    # Bottleneck
-    ## Store output of subsequent dense block; upsample only these new features
+    # bottleneck
+    ## store output of subsequent dense block; upsample only these new features
     block_to_upsample = []
-    # Dense Block
+    # dense Block
     for j in range(n_layers_per_block[n_pool]):
         l = BN_ReLU_Conv(Tiramisu, growth_rate, dropout_p = dropout_p)
         block_to_upsample.append(l)
         Tiramisu = ks.layers.concatenate([Tiramisu, l])
     
-    # Upsampling path
+    # upsampling path
     for i in range(n_pool):
         ## Transition Up ( Upsampling + concatenation with the skip connection)
         n_filters_keep = growth_rate * n_layers_per_block[n_pool + i]
         Tiramisu = TransitionUp(skip_connection_list[i], block_to_upsample,
                              n_filters_keep)
-        ## Dense Block
+        ## dense Block
         block_to_upsample = []
         for j in range(n_layers_per_block[n_pool + i + 1]):
             l = BN_ReLU_Conv(Tiramisu, growth_rate, dropout_p = dropout_p)
             block_to_upsample.append(l)
             Tiramisu = ks.layers.concatenate([Tiramisu, l])
     
-    # Output layer; 1x1 convolution, m = number of classes
+    # output layer; 1x1 convolution, m = number of classes
     outputz = ks.layers.Conv2D(n_classes, 1, activation = "softmax")(Tiramisu)
     
     model = tf.keras.Model(inputs = [inputz], outputs = [outputz])
@@ -128,38 +130,8 @@ def FCDense(n_classes, input_shape = (imgr, imgc, imgdim),
     print(f'Total number of layers: {len(model.layers)}')
     return model
 
-# Get model
+# get model
 model = FCDense(n_classes = N_CLASSES)
 
-# Using some simple built-in learning rate decay:
-lr_sched = ks.optimizers.schedules.ExponentialDecay(
-    initial_learning_rate = 1e-3,
-    # decay after n steps
-    decay_steps = np.floor(N_img/bs),
-    decay_rate = 0.995)
-optimizer = ks.optimizers.RMSprop(learning_rate = lr_sched)# FC-DenseNet Optim.
-
-# list callbacks
-logdir = os.path.join(dir_out("logs"), datetime.datetime.now() \
-                      .strftime("%y-%m-%d-%H-%M-%S"))
-os.makedirs(logdir)
-os.chdir(logdir)
-cllbs = [
-    ks.callbacks.EarlyStopping(patience = 8),
-    ks.callbacks.ModelCheckpoint(dir_out("Checkpoint.h5"),
-                                 save_best_only = True),
-    ks.callbacks.TensorBoard(log_dir = logdir)
-    ]
-# compile model
-model.compile(optimizer = optimizer, loss = "sparse_categorical_crossentropy",
-              metrics = ["accuracy"])
-model.summary()
-
-model.fit(train_generator, epochs = 45, steps_per_epoch = np.ceil(N_img/bs),
-                 validation_data = val_generator,
-                 validation_steps = np.ceil(N_val/bs),
-                 callbacks = cllbs)
-os.chdir(dir_out())
-# save model
+# directory to save model
 os.makedirs(dir_out("mod_FCD"), exist_ok = True)
-model.save(dir_out("mod_FCD"))
