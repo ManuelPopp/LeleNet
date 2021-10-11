7eb53177ca3dd9b6d4e8beeb59f6513ab90ab512
Manuel Popp <34488159+ManuelPopp@users.noreply.github.com>
2021-10-11 23:15:59 +0200

    Update GeoRef.bat

--- bat/GeoRef.bat
+++ bat/GeoRef.bat
@@ -1,1 +1,9 @@
-py C:\Users\Manuel\Nextcloud\Masterarbeit\py3\fun\ApproxGeoRef.py "F:\Block_4\Block4_5\Block4_5" "F:\Block_4\Block4_5GeoTIFF"
+@echo off
+set /p b="Select block: "
+set /p p="Select plot: "
+set in = "F:\Block_%b%\Block%b%_%p%\Block%b%_%p"
+set out = "F:\Block_%b%\Block%b%_%p%GeoTIFF"
+@echo Started: %date% %time%
+py C:\Users\Manuel\Nextcloud\Masterarbeit\py3\fun\ApproxGeoRef.py in out
+@echo Completed: %date% %time%
+pause
