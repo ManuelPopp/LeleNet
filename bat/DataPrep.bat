8315726e778b055576612b0d24300e2af52fd09c
Manuel Popp <34488159+ManuelPopp@users.noreply.github.com>
2021-10-11 22:57:37 +0200

    Update DataPrep.bat

--- bat/DataPrep.bat
+++ bat/DataPrep.bat
@@ -1,5 +1,5 @@
-@echo off
 set /p imgdims="Enter tile dimensions (px in one dimension): "
+@echo Started: %date% %time%
 py C:\Users\Manuel\Nextcloud\Masterarbeit\py3\DataPreparation.py -imgd %imgdims%
-echo "Tiles created."
+@echo Completed: %date% %time%
 pause
