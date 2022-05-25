2960f4788dcaf6d6956d3f79861ac17776d5c112
Manuel Richard Popp <34488159+ManuelPopp@users.noreply.github.com>
2022-05-25 09:30:47 +0200

    Update submit_0.sh

--- bat/lnx/submit_0.sh
+++ bat/lnx/submit_0.sh
@@ -13,7 +13,12 @@
 #
 module purge
 module load devel/cuda/11.4
+mkdir -p $TMP/dat/{tls,xls}
+cp -r $HOME/LeleNet/dat/tls/512_n_class $TMP/dat/tls/03_2021
+cp $HOME/LeleNet/dat/xls/SpeciesList.xlsx $TMP/dat/xls/SpeciesList.xlsx
 source $HOME/venv/bin/activate
-python3 $HOME/LeleNet/py3/LeleNet_trn.py "dl3" 5 50 -yr "512_3_class_Ld" -ww 1.0 -op "adam" -lc 1
+python3 $HOME/LeleNet/py3/LeleNet_trn.py "unet" 8 45 -dd $TMP -ww 0.25 -op "rms" -dr 0.0 -lr 1e-4 -out "UNet0_025" -tbn "UNet0025"
+python3 $HOME/LeleNet/py3/LeleNet_trn.py "unet" 8 45 -dd $TMP -ww 0.50 -op "rms" -dr 0.0 -lr 1e-4 -out "UNet0_050" -tbn "UNet0050"
+python3 $HOME/LeleNet/py3/LeleNet_trn.py "unet" 8 45 -dd $TMP -ww 1.50 -op "rms" -dr 0.0 -lr 1e-4 -out "UNet0_150" -tbn "UNet0150"
 #python3 $HOME/LeleNet/py3/XX_Testing_and_visualisation.py
 deactivate
