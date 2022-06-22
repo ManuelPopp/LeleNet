89bb1b54e6b7503124d1f694f26182a8d5e058dc
Manuel Richard Popp <34488159+ManuelPopp@users.noreply.github.com>
2022-06-22 23:32:39 +0200

    Create clear_TensorBoard.sh

---
+++ bat/lnx/clear_TensorBoard.sh
@@ -0,0 +1,2 @@
+AllExp = tensorboard dev list | grep https://tensorboard.dev/experiment/ | sed "s|https://tensorboard.dev/experiment/||g" | tr '\n' ',' | sed 's/\///g'
+tensorboard dev delete $AllExp
