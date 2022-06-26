6b59a6c5d8c2ca050f6020bccbf4dfb9573fe360
Manuel Richard Popp <34488159+ManuelPopp@users.noreply.github.com>
2022-06-26 13:58:50 +0200

    Update Predict.bat

--- bat/win/Predict.bat
+++ bat/win/Predict.bat
@@ -1,17 +1,23 @@
-set omk_dir=G:\Masterarbeit\dat\test_1024_21\
-set out_dir=G:\Masterarbeit\dat\test_1024_21\
+set omk_dir=G:\Masterarbeit\dat\test_
+set out_dir=G:\Masterarbeit\dat\test_
 set mod_dir=C:\Users\Manuel\Dropbox\Models\
 set mod_subdir=\cpts\trained_mod
 set /p omk_name="Enter orthomosaic filename: "
 set /p out_name="Enter output filename: "
 set /p mod_name="Enter model name: "
-set omk_path=%omk_dir%%omk_name%
-set out_path=%out_dir%%out_name%
+set /p gpu_use="Use GPU (y/n): "
+IF "%gpu_use%"=="y" (
+    set ugpu==True
+) ELSE (
+    set ugpu==False
+)
+set omk_path=%omk_dir%%mod_name%\%omk_name%.tif
+set out_path=%out_dir%%mod_name%\%out_name%.tif
 set mod_path=%mod_dir%%mod_name%%mod_subdir%
 @echo Started: %date% %time%
 @echo Input: %omk_path%
 @echo Output: %out_path%
 @echo Model: %mod_path%
-py D:\Dateien\Studium_KIT\Master_GOEK\Masterarbeit\py3\LeleNet_prd.py %omk_path% %out_path% %mod_path% -rol 0.25 -col 0.25
+py D:\Dateien\Studium_KIT\Master_GOEK\Masterarbeit\py3\LeleNet_prd.py %omk_path% %out_path% %mod_path% -rol 0.25 -col 0.25 -gpu %ugpu%
 @echo Completed: %date% %time%
 pause
