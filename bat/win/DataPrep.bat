decff22f22e1249f5fb69496de7f8a7fac41f92b
Manuel Richard Popp <34488159+ManuelPopp@users.noreply.github.com>
2022-07-01 11:10:27 +0200

    Delete DataPrep.bat

--- bat/win/DataPrep.bat
+++
@@ -1,6 +0,0 @@
-set /p imgdims="Enter tile dimensions (px in one dimension): "
-set /p dname="Enter dataset main folder name: "
-@echo Started: %date% %time%
-py D:\Dateien\Studium_KIT\Master_GOEK\Masterarbeit\py3\DataPreparation.py -name %dname% -imgd %imgdims% -grp True -abc -1 -wd home
-@echo Completed: %date% %time%
-pause
