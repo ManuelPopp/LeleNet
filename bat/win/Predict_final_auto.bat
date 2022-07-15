d03b07aefd4bdb561e3844f1a07cbe7c0c6dca9c
Manuel Richard Popp <34488159+ManuelPopp@users.noreply.github.com>
2022-07-15 12:38:31 +0200

    Add files via upload

---
+++ bat/win/Predict_final_auto.bat
@@ -0,0 +1,42 @@
+set omk_dir=G:\Masterarbeit\dat\test_
+set out_dir=G:\Masterarbeit\dat\test_
+set mod_dir=G:\Masterarbeit\out\
+set mod_subdir=\cpts\trained_mod
+set /p mod_name="Enter model name: "
+set /p epoch="Enter best epoch: "
+set /p gpu_use="Use GPU (y/n): "
+IF "%gpu_use%"=="y" (
+    set ugpu= -gpu
+) ELSE (
+    set ugpu=
+)
+set mod_path=%mod_dir%%mod_name%%mod_subdir%\Epoch.%epoch%.hdf5
+@echo Started: %date% %time%
+set tstart=%time%
+set omk_name=ortho_B4_1_CROP
+set out_name=%omk_name:CROP=PRED%
+set omk_path=%omk_dir%%mod_name%\%omk_name%.tif
+set out_path=%out_dir%%mod_name%\%out_name%.tif
+@echo Input: %omk_path%
+@echo Output: %out_path%
+@echo Model: %mod_path%
+py D:\Dateien\Studium_KIT\Master_GOEK\Masterarbeit\py3\LeleNet_prd.py %omk_path% %out_path% %mod_path% -rol 0.25 -col 0.25%ugpu%
+set omk_name=ortho_B6_2_CROP
+set out_name=%omk_name:CROP=PRED%
+set omk_path=%omk_dir%%mod_name%\%omk_name%.tif
+set out_path=%out_dir%%mod_name%\%out_name%.tif
+@echo Input: %omk_path%
+@echo Output: %out_path%
+@echo Model: %mod_path%
+py D:\Dateien\Studium_KIT\Master_GOEK\Masterarbeit\py3\LeleNet_prd.py %omk_path% %out_path% %mod_path% -rol 0.25 -col 0.25%ugpu%
+set omk_name=ortho_B7_5_CROP
+set out_name=%omk_name:CROP=PRED%
+set omk_path=%omk_dir%%mod_name%\%omk_name%.tif
+set out_path=%out_dir%%mod_name%\%out_name%.tif
+@echo Input: %omk_path%
+@echo Output: %out_path%
+@echo Model: %mod_path%
+py D:\Dateien\Studium_KIT\Master_GOEK\Masterarbeit\py3\LeleNet_prd.py %omk_path% %out_path% %mod_path% -rol 0.25 -col 0.25%ugpu%
+set tend=%time%
+@echo Started %tstart%, completed %tend% on %date%.
+pause
