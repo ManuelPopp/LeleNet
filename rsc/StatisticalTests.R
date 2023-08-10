b4de7ce098c9af2f42668854b662f35fcb07f79a
Manuel Richard Popp <34488159+ManuelPopp@users.noreply.github.com>
2023-08-10 22:14:34 +0200

    Update StatisticalTests.R

--- rsc/StatisticalTests.R
+++ rsc/StatisticalTests.R
@@ -15,7 +15,7 @@ for(i in 1:NROW(packages)){
   }
 }
 
-packages <- c("readxl", "dplyr", "reshape2", "ggplot2")
+packages <- c("readxl", "dplyr", "reshape2", "ggplot2", "raster", "ggplot2", "landscapemetrics")
 for(i in 1:NROW(packages)){
   if(!require(packages[i], character.only = TRUE)){
     install.packages(packages[i])
@@ -52,6 +52,8 @@ dat <- as.data.frame(readxl::read_excel(file.path(excel_path, "Accuracy_table.xl
 names(dat) <- c("Class", paste0(rep(c("U", "F", "D"), each = 3), rep(c(256, 512, 1024), 3)),
                 "a512", "b512")
 dat[dat == "-"] <- NA
+
+# Remove Mundulea sericea, since the species was not found in any of the training plots
 dat <- dat[-which(dat$Class == "Mundulea sericea"),]
 for(j in 2:ncol(dat)){
   dat[, j] <- as.numeric(dat[, j])
@@ -86,60 +88,108 @@ colnames(t_tests_t) <- models[-9]
 shapiro_mat <- t_tests_p
 
 for(i in 1:(nrow(t_tests_p))){
-  for(j in 1:(ncol(t_tests_p))){
-    t_test <- t.test(dat[, which(names(dat) == row.names(t_tests_p)[i])],
-                     dat[, which(names(dat) == colnames(t_tests_p)[j])],
-                     paired = TRUE)
-    
-    Difference = dat[, which(names(dat) == row.names(t_tests_p)[i])] -
-      dat[, which(names(dat) == colnames(t_tests_p)[j])]
-    
-    hist(Difference,   
-         col = "gray", 
-         main = "Histogram of differences",
-         xlab = "Difference")
-    
-    if(i >= j){
-      t_tests_p[i, j] <- signif(t_test$p.value, 3)
-      t_tests_t[i, j] <- signif(t_test$statistic, 3)
-      
-      shapwilk <- shapiro.test(Difference)
-      shapiro_mat[i, j] <- round(shapwilk$p.value, 2)
-      
-    }else{
-      t_tests_p[i, j] <- NA
-      t_tests_t[i, j] <- NA
-      shapiro_mat[i, j] <- NA
+    for(j in 1:(ncol(t_tests_p))){
+        t_test <- t.test(dat[, which(names(dat) == row.names(t_tests_p)[i])],
+                         dat[, which(names(dat) == colnames(t_tests_p)[j])],
+                         paired = TRUE)
+        
+        Difference = dat[, which(names(dat) == row.names(t_tests_p)[i])] -
+            dat[, which(names(dat) == colnames(t_tests_p)[j])]
+        
+        hist(Difference,   
+             col = "gray", 
+             main = "Histogram of differences",
+             xlab = "Difference")
+        
+        if(i >= j){
+            if(t_test$p.value < 0.001){
+                t_tests_p[i, j] <- "\\textless 0.001"
+            }else if(t_test$p.value < 0.01){
+                t_tests_p[i, j] <- as.character(round(t_test$p.value, 3))
+            }else{
+                t_tests_p[i, j] <- as.character(round(t_test$p.value, 2))
+            }
+            
+            t_tests_t[i, j] <- as.character(signif(t_test$statistic, 3))
+            
+            shapwilk <- shapiro.test(Difference)
+            shapiro_mat[i, j] <- round(shapwilk$p.value, 2)
+            
+        }else{
+            t_tests_p[i, j] <- " "
+            t_tests_t[i, j] <- " "
+            shapiro_mat[i, j] <- NA
+        }
     }
-  }
 }
 
+# Gray out all instances where differences were not normally distributed according to the Shapiro-Wilk-Test
+t_tests_p[which(shapiro_mat < 0.05)] <- paste0(
+    "\\color[gray]{0.5} ",
+    t_tests_p[which(shapiro_mat < 0.05)]
+    )
+
+t_tests_t[which(shapiro_mat < 0.05)] <- paste0(
+    "\\color[gray]{0.5} ",
+    t_tests_t[which(shapiro_mat < 0.05)]
+)
+
 w_tests_p <- matrix(nrow = length(models) - 1, ncol = length(models) - 1)
-w_tests_t <- matrix(nrow = length(models) - 1, ncol = length(models) - 1)
+w_tests_w <- matrix(nrow = length(models) - 1, ncol = length(models) - 1)
 
 row.names(w_tests_p) <- models[-1]
 colnames(w_tests_p) <- models[-9]
-row.names(w_tests_t) <- models[-1]
-colnames(w_tests_t) <- models[-9]
+row.names(w_tests_w) <- models[-1]
+colnames(w_tests_w) <- models[-9]
 
 for(i in 1:(nrow(w_tests_p))){
-  for(j in 1:(ncol(w_tests_p))){
-    w_test <- wilcox.test(dat[, which(names(dat) == row.names(w_tests_p)[i])],
-                     dat[, which(names(dat) == colnames(w_tests_p)[j])],
-                     paired = TRUE, alternative = "two.sided")
-    
-    if(i >= j){
-      w_tests_p[i, j] <- signif(w_test$p.value, 3)
-      w_tests_t[i, j] <- signif(w_test$statistic, 3)
-      
-    }else{
-      w_tests_p[i, j] <- NA
-      w_tests_t[i, j] <- NA
+    for(j in 1:(ncol(w_tests_p))){
+        if(i >= j){
+            w_test <- wilcox.test(dat[, which(names(dat) == row.names(w_tests_p)[i])],
+                                  dat[, which(names(dat) == colnames(w_tests_p)[j])],
+                                  paired = TRUE, alternative = "two.sided")
+            
+            if(w_test$p.value < 0.001){
+                w_tests_p[i, j] <- "\\textless 0.001"
+            }else if(w_test$p.value < 0.01){
+                w_tests_p[i, j] <- as.character(round(w_test$p.value, 3))
+            }else{
+                w_tests_p[i, j] <- as.character(round(w_test$p.value, 2))
+            }
+            w_tests_w[i, j] <- as.character(signif(w_test$statistic, 3))
+            
+        }else{
+            w_tests_p[i, j] <- " "
+            w_tests_w[i, j] <- " "
+        }
     }
-  }
 }
 
-write.table(t_tests_p, file.path(excel_path, "Paired_t_tests_p.csv"), sep = " & ", eol = "\\\\\n")
-write.table(t_tests_t, file.path(excel_path, "Paired_t_tests_t.csv"), sep = " & ", eol = "\\\\\n")
-write.table(w_tests_p, file.path(excel_path, "Wilcox_tests_p.csv"), sep = " & ", eol = "\\\\\n")
-write.table(w_tests_t, file.path(excel_path, "Wilcox_tests_t.csv"), sep = " & ", eol = "\\\\\n")
+t_tests_p <- cbind(c(512, 1024, 256, 512, 1024, 256, 512, 1024), t_tests_p)
+colnames(t_tests_p)[1] <- "Tile size"
+
+t_tests_t <- cbind(c(512, 1024, 256, 512, 1024, 256, 512, 1024), t_tests_t)
+colnames(t_tests_t)[1] <- "Tile size"
+
+w_tests_p <- cbind(c(512, 1024, 256, 512, 1024, 256, 512, 1024), w_tests_p)
+colnames(w_tests_p)[1] <- "Tile size"
+
+w_tests_w <- cbind(c(512, 1024, 256, 512, 1024, 256, 512, 1024), w_tests_w)
+colnames(w_tests_w)[1] <- "Tile size"
+
+# Export to LaTeX-like tables
+write.table(t_tests_p, file.path(excel_path, "Paired_t_tests_p.csv"), sep = " & ", eol = "\\\\\n",
+            row.names = TRUE, quote = FALSE
+            )
+
+write.table(t_tests_t, file.path(excel_path, "Paired_t_tests_t.csv"), sep = " & ", eol = "\\\\\n",
+            row.names = TRUE, quote = FALSE
+)
+
+write.table(w_tests_p, file.path(excel_path, "Wilcox_tests_p.csv"), sep = " & ", eol = "\\\\\n",
+            row.names = TRUE, quote = FALSE
+)
+
+write.table(w_tests_w, file.path(excel_path, "Wilcox_tests_w.csv"), sep = " & ", eol = "\\\\\n",
+            row.names = TRUE, quote = FALSE
+)
